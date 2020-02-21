991ea77fae437cd1a367fc591d4090e52eb91c28
dKharchi <djaouidadja@gmail.com>
2020-02-21 16:25:45 +0000

    21 fev 2020

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+//init app
+const app = express()
+
+// connect to mongodb
+app.use(bodyParser.json())
+
+// init routes
+//app.use('/api', require('./routes/api'))
+
+
+app.listen(process.env.port || 4000, function () {
+    console.log('ready for  requests')
+})
---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = exportress.Router()
+
+
+//const Job = ''
+
+
+//get all jobs
+//router.get
+
+//get one job
+//router.get
+
+//add new job
+//router.post
+
+//update job
+//router.put
+
+//delete a job
+//router.delete
